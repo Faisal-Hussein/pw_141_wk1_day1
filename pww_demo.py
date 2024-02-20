11b6d5daec78be61dfcb3b8af1fcadeb181938f6
Faisal-Hussein <fh069017@ohio.edu>
2024-02-19 20:49:27 -0500

    first

---
+++ pww_demo.py
@@ -0,0 +1,11 @@
+print("hey man")
+name = "faisal"
+
+print ("Hey " + name)
+
+def namer(f_name):
+    print(f"welcome to CT {f_name}")
+    print("this will run")
+namer("faisal")
+namer("adam")
+namer("daniel")
